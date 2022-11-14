944688cc173dca58fdd74177dcead90a9054d017
sivasailakshmiravi <sailakshmigudupu6@gmail.com>
2022-11-14 12:58:19 +0530

    created new file to add two numbers

--- cal.cpp
+++ cal.cpp
@@ -9,13 +9,30 @@ class Cal{
 			y=0;
 		}
 		int add(int,int);
+		int sub(int,int);
+		int mul(int,int);
+		int div(int,int);
 };
 int Cal::add(int x, int y){
 	return x+y;
 }
+int Cal::sub(int x, int y){
+        return x-y;
+}
+int Cal::mul(int x, int y){
+        return x*y;
+}
+int Cal::div(int x, int y){
+	if (y>0)
+        return x/y;
+}
+
 int main()
 {       Cal obj;
 	cout<<"add is "<<obj.add(4,6)<<endl;
+	cout<<"add is "<<obj.sub(4,6)<<endl;
+	cout<<"add is "<<obj.mul(4,6)<<endl;
+	cout<<"add is "<<obj.div(4,6)<<endl;
 	cout<<"sailu "<<endl;
 	return 0;
 }
